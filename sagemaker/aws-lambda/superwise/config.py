f28a3bfeb3480a78b69d22b01bfb06463a948597
oritzahary <or.itzary@superwise.ai>
2021-12-04 18:37:13 +0200

    Remove aws lambda code

--- sagemaker/aws-lambda/superwise/config.py
+++
@@ -1,7 +0,0 @@
-import os
- 
-
-class Config:
-    FRONTEGG_URL = os.environ.get("FRONTEGG_SUPERWISE_URL", "https://auth.superwise.ai")
-    SUPERWISE_HOST = os.environ.get("SUPERWISE_HOST", "portal.superwise.ai")
-    POOLING_INTERVAL_SEC = 15
